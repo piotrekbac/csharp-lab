219c7de042b9d828b250820f88e4cd36205160ae
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2024-10-22 16:34:14 +0200

    clean2

--- Lab01/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿Console.WriteLine("Przejście do\nnowej linii.");
-Console.WriteLine("Tabulacja\tpozioma.");
-Console.WriteLine("Tabulacja\npionowa.");
-Console.WriteLine("Alarm!\a");
-Console.WriteLine("Pojedynczy apostrof: \'");
-Console.WriteLine("Pojedynczy cudzysłów: \"");
-Console.WriteLine("Znak pusty: \0");
