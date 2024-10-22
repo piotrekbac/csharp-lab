e16d7f893c0e35af7462d655d32965090c54a04b
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2024-10-22 16:35:13 +0200

    clean3

---
+++ Lab03/Lab03.cs
@@ -0,0 +1,59 @@
+﻿// Przykład tłumaczący 
+
+//int n = 0;
+//do
+//{
+//    Console.Write(n);
+//    n++;
+//} while (n < 5); 
+
+//wyjście (czyli co otrzymamy): 01234
+
+// Zadanie 1 
+
+//int n = 10;
+//do
+//{
+//    Console.Write(n);
+//    n--;
+
+//} while (n > -1);
+
+// Koniec zadania 1 
+
+// Zadanie 2 
+
+//string haslo; 
+
+//do
+//{
+//    Console.Write("Proszę podać hasło: ");
+//    haslo = Console.ReadLine();
+//} while (haslo != "koniec");
+
+// Koniec zadania 2 
+
+// Przykład tłumaczący 
+
+//int n = 0; 
+//while (n < 5)
+//{
+//    Console.Write(n);
+//    n++;
+//}
+
+//Wyjście (czyli co otrzymamy): 01234
+
+// Zadanie 3 
+
+int n = 1;
+while (n <= 101)
+{
+    if (n % 2 == 0)
+    {
+        Console.WriteLine(n);
+    }
+    n++;
+}
+
+// Koniec zadania 3 
---
+++ Lab03/Lab03/Lab03/Program.cs
@@ -0,0 +1,93 @@
+﻿// Przykład tłumaczący 
+
+//int n = 0;
+//do
+//{
+//    Console.Write(n);
+//    n++;
+//} while (n < 5); 
+
+//wyjście (czyli co otrzymamy): 01234
+
+// Zadanie 1 
+
+//int n = 10;
+//do
+//{
+//    Console.Write(n);
+//    n--;
+
+//} while (n > -1);
+
+// Koniec zadania 1 
+
+// Zadanie 2 
+
+//string haslo; 
+
+//do
+//{
+//    Console.Write("Proszę podać hasło: ");
+//    haslo = Console.ReadLine();
+//} while (haslo != "koniec");
+
+// Koniec zadania 2 
+
+// Przykład tłumaczący 
+
+//int n = 0; 
+//while (n < 5)
+//{
+//    Console.Write(n);
+//    n++;
+//}
+
+//Wyjście (czyli co otrzymamy): 01234
+
+// Zadanie 3 
+
+//int n = 1;
+//while (n <= 101)
+//{
+//    if (n % 2 == 0)
+//    {
+//        Console.WriteLine($"Liczby od 1 do 100 parzyste: {n}");
+//    }
+//    n++;
+//}
+
+// Koniec zadania 3 
+
+// Przykład tłumaczący 
+
+//for (int i = 0; i < 3; i++)
+//    {
+//    Console.Write(i);
+//    }
+
+//Wyjśćie (czyli co otrzymamy): 012
+
+// Zadanie 4 
+
+//for (int i = 0; i <= 15; i++)
+//{
+//    int potega = i * i;  
+//    Console.WriteLine($"Podniesienie do potęgi 2 liczby od 1 do 15: {potega}");
+//}
+
+// Koniec zadania 4 
+
+// Przykład tłumaczący 
+
+//List<int> fibNumbers = new() { 0, 1, 1, 2, 3, 5, 8, 13 };
+//foreach (int element in fibNumbers)
+//{
+//    Console.WriteLine($" {element} ");
+//}
+////Wyjście (czyli co otrzymamy): 0 1 1 2 3 5 8 13 
+
+// Zadanie 5 
+
+
+
+// Koniec zadania 5 
