0d89611e36c3b5d993290e65174b71a5de3fe45d
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2024-11-05 17:51:05 +0100

    clean 6

--- Lab03/Lab03/Lab03/Program.cs
+++ Lab03/Lab03/Lab03/Program.cs
@@ -58,6 +58,19 @@
 
 // Koniec zadania 3 
 
+// Sposób prowadzącego na zadanie 3 
+
+//int j = 1;
+//Console.WriteLine("Liczby parzyste: ");
+//while (j <= 100)
+//{
+//    if (j % 2 == 0)
+//        Console.WriteLine(j);
+//    j++;
+//}
+
+// Koniec sposobu prowadzącego na zadanie 3 
+
 // Przykład tłumaczący 
 
 //for (int i = 0; i < 3; i++)
@@ -77,6 +90,17 @@
 
 // Koniec zadania 4 
 
+// Wg wykładowcy - zadanie 4A 
+//for (int i = 1; int <= 5; int++)
+//{
+//    Console.WriteLine($"Square of {i} equals { i* i}");
+//}
+//// oraz zadanie 4B 
+//for (int i = 1; i <= 15;  i++)
+//{
+//    string txt = "Swuare of " + i + " equals " + i * i; 
+//}
+
 // Przykład tłumaczący 
 
 //List<int> fibNumbers = new() { 0, 1, 1, 2, 3, 5, 8, 13 };
@@ -88,6 +112,220 @@
 
 // Zadanie 5 
 
+//List<int> numbers = new List<int>() { 4, 25, 35, 42, 55, 78, 99, 101 }; 
 
+//foreach (int element in numbers)
+//{
+//    Console.WriteLine($" Czy dane elementy ze zbioru 4,25,35,42,55,78,99,101 są podzielne przez 7: {element % 7 == 0}");
+//}
 
 // Koniec zadania 5 
+
+// Zadanie 6 
+
+//List<int> numbers = new List<int>() { 4, 25, 35, 42, 55, 78, 99, 101 };
+
+//foreach (int element in numbers)
+//{
+
+//    if (element % 7 == 0)
+//    {
+//        Console.WriteLine(element);
+//        break;
+//    }
+
+//}
+
+// Koniec zadania 6 
+
+// Zadanie 7 
+
+//double[] tablicaDouble = new double[20];
+
+//for (int i = 0; i < 20; i++)
+//{
+//    tablicaDouble[i] = Math.Sqrt(i);
+//    Console.WriteLine(tablicaDouble[i]);
+//}
+
+// Koniec zadania 7 
+
+// Zadanie 8 
+
+//int [,] macierz = new int[3, 4];
+
+//int liczba = 1;
+
+//for (int i = 0; i < 3; i++)
+//{
+//    for (int j = 0; j < 4; j++)
+//    {
+//        macierz[i, j] = liczba++;
+
+//    }
+
+//}
+
+//// Ukazanie macierzy po transpozycji 
+
+//for (int i = 0; i < 3; i++)
+//{
+//    for (int j = 0; j < 4; j++)
+//    {
+
+//        Console.WriteLine(macierz[i,j] + "\t");
+
+//    }
+//    Console.WriteLine(); 
+//}
+
+// Koniec zadania 8 - PATRZ ZDJ 
+
+// Zadnaie 10
+
+//using System;
+//using System.Security;
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        string tekst = "Przykładowy tekst";
+//        WypiszWielkimiLiterami(tekst);
+//    }
+
+//    static void WypiszWielkimiLiterami(string tekst)
+//    {
+//        Console.WriteLine(tekst.ToUpper());
+//    }
+
+//}
+
+// Koniec zadania 10 - ZDJ 
+
+// Zadanie 11 
+//using System;
+
+//using System;
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        // Przykładowe wywołanie procedury
+//        int x = 3; // liczba wierszy
+//        int y = 4; // liczba kolumn
+//        ZbudujIMacierz(x, y);
+//    }
+
+//    // Definicja procedury
+//    static void ZbudujIMacierz(int x, int y)
+//    {
+//        // Deklaracja macierzy o wymiarach X na Y
+//        char[,] macierz = new char[x, y];
+
+//        // Wypełnianie macierzy znakami '$'
+//        for (int i = 0; i < x; i++)
+//        {
+//            for (int j = 0; j < y; j++)
+//            {
+//                macierz[i, j] = '$';
+//            }
+//        }
+
+//        // Wyświetlanie macierzy
+//        for (int i = 0; i < x; i++)
+//        {
+//            for (int j = 0; j < y; j++)
+//            {
+//                Console.Write(macierz[i, j] + " ");
+//            }
+//            Console.WriteLine();
+//        }
+//    }
+//}
+
+// Koniec zadania 11 
+
+// Zadanie 11 - wg wykładowcy 
+
+void CreateAndPrintMatrix(int x, int y)
+{
+    // Stwórz macierz
+    char[,] matrix = new char[x,y];
+
+    // Wypełnij macierz
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            matrix[x,y] = '#';
+        }
+    }
+
+    // Wydrukuj macierz
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            Console.WriteLine(matrix[x,y]);
+        }
+        Console.WriteLine();
+    }
+
+}
+CreateAndPrintMatrix(10, 10);
+
+
+// Koniec zadania 11 - wg Wykładowcy 
+
+
+// Zadanie 12 - wg wykładowcy  
+
+//string EvenOrOdd1(int toCheck)
+//{
+//    string retStr = " ";
+
+//}
+
+// Koniec zadania 12 - wg wykładowcy 
+
+
+//string EvenOrOdd3 (int toCheck)
+//{
+//    return toCheck % 2 == 0 ? "Even" : "Odd";
+//}
+
+// Koniec zadania 12 
+
+// zadanie 13 wg wykładowcy - PRZEPISZ NA SPOKOJNIE ZE ZDJ 
+
+//void PrintConsoleSquare(int x, int y)
+//{
+//    // rysuj górną krawędź 
+//    for (int i = 0; i < x; i++)
+//        Console.Write("$");
+//    // zejdź do nowej linii 
+//    Console.WriteLine();
+
+//    // rysuj środek (boki) - dla kazdego wiersza
+//    for (int i = 0; i < y - 2; i++)
+//    {
+//        Console.Write("$");
+
+//        //teraz drukuj proste 
+//        for (int ij = 0; i < x - 2; i++)
+//        {
+//            Console.WriteLine("$");
+//        }
+//    }
+
+//    // rysuj dolną krawędź
+//    for (int i = 0; i < x; i++)
+//    {
+//        Console.Write("$");
+//    }
+//    PrintConsoleSquare(5, 5);
+//}
+
+// koniec 13 wg wykładowcy 
---
+++ Lab04/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,303 @@
+﻿//Przykład tłumaczący
+
+//double Add(double a = 0.0, double b = 0.0)
+//{
+//    return a + b;
+//}
+
+//Zad1 
+
+//static double QuadraticEquation(double x = 0.0, double a = 1.0, double b = 1.0, double c = 1.0)
+//{
+//    return x * x * a + b * x + c;
+//}
+
+//Console.WriteLine("Wartość równania kwadratowego (2.0,3.0,1.0): " + QuadraticEquation(2.0, 3.0, 1.0));
+
+//Koniec Zad1
+
+//Przykład tłumaczący 
+
+//int Add(int a, int b)
+//{
+//    return a + b;
+//}
+
+//double Add(double a, double b, double c)
+//{
+//    return a + b + c;
+//}
+
+//Zad2 
+
+//using System; 
+
+//class Program
+//{ 
+//    static void Main()
+//    {
+//        void Swap(ref double a, ref double b)
+//        {
+//            double temp = a;
+//            a = b;
+//            b = temp;
+//        }
+//        double x = 10;
+//        double y = 20;
+//        Swap(ref x,ref y);
+//        Console.WriteLine($"x = {x}, y = {y}");
+//    }
+
+//}
+
+//Koniec Zad2 
+
+//Przykład tłumaczący 
+//enum Season
+//{
+//    Spring,
+//    Summer,
+//    Autumn,
+//    Winter
+//}
+
+//enum ErrorCode : ushort
+//{
+//    None = 0,
+//    Unknown = 1,
+//    ConnectionLost = 100,
+//    OutlierReading = 200
+//}
+
+
+//Zad3
+
+//using System;
+
+//class Program
+//{
+
+//    public static class Multiplier
+//    {
+
+
+
+//        static double Multiply(double a, double b)
+//        {
+//            return a * b;
+//        }
+
+//        static double Multiply(double a, double b, double c)
+//        {
+//            return a * b * c;
+//        }
+
+//        static double Multiply(params double[] a)
+//        {
+//            double result = 1.0;
+//            foreach (double x in a)
+//            {
+//                result *= x;
+//            }
+//            return result;
+//        }
+//    }
+//}
+
+//Koniec Zad3
+//Zad4 
+
+//using System;
+//using System.Linq.Expressions;
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        PrintDayOfTheWeek(DayOfTheWeek.Monday);
+//        PrintDayOfTheWeek(DayOfTheWeek.Sunday);
+
+//    }
+//    enum DayOfTheWeek
+//    {
+//        Monday = 1,
+//        Tuesday = 2,
+//        Wednesday = 3,
+//        Thursday = 4,
+//        Friday = 5,
+//        Saturday = 6,
+//        Sunday = 7,
+//    }
+
+//    static void PrintDayOfTheWeek(DayOfTheWeek day)
+//    {
+
+//        Console.WriteLine(day);
+//        Console.WriteLine((int)day);
+//        if ((int)day == 6 || (int)day == 7)
+//        {
+//            Console.WriteLine("It's the weekend");
+//        }
+//        else
+//        {
+//            Console.WriteLine("It's the workday");
+//        }
+//    }
+//}
+
+
+//Koniec Zad4 
+
+//Przykład tłumaczący 
+
+//string itemName = "eggplant";
+//DateTime date = DateTime.Now;
+//decimal price = 1.99m;
+//Unit unit = Unit.item;
+//Console.WriteLine($"On {date.Year}, the price of {itemName} was {price} per {unit}.");
+
+//Zad 5 
+
+//DateTime date = DateTime.Now;
+
+//Console.WriteLine($"Nasze dane z daty: Dzień: {date.Day}, Miesiąc: {date.Month}, Rok: {date.Year}, Godzina: {date.Hour}, Minuta: {date.Minute}, Sekunda: {date.Second}, Dzień tygodnia: {date.DayOfWeek}");
+
+
+//Koniec Zad5 
+
+//Przykład tłumaczący 
+
+//try
+//{
+//    // Code to try goes here.
+//}
+//catch (SomeSpecificException ex)
+//{
+//    // Code to handle the specific exception goes here.
+//}
+//catch (Exception ex)
+//{
+//    // Code to handle all other exceptions goes here.
+//}
+//finally
+//{
+//    // Code to execute after the try (and possibly catch) blocks
+//    // goes here.
+//}
+
+//Zad 6 
+
+//using System;
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        double dzielna = 10;
+//        double dzielnik = 5;
+//        double wynik = dzielna / dzielnik;
+//        Console.WriteLine("Wynik:" + wynik);
+
+//    }
+
+//    static double Wynik(double dzielna, double dzielnik)
+//    {
+//        try
+//        {
+//            return dzielna / dzielnik; 
+//        }
+
+//        catch(Exception e)
+//        {
+//            Console.WriteLine("Błąd: " + e.Message);
+//            return 0;
+//        }
+//        finally
+//        {
+//            Console.WriteLine("Wynik naszego dzielenia wynosi: ");
+//        }
+//    }
+//}
+
+
+//Koniec Zad6
+
+//Przykład tłumaczący 
+
+//bool CheckTheAge(int age)
+//{
+//    if (age < 0)
+//        throw new ArgumentOutOfRangeException("Age can not be a negative number!");
+//    if (age >= 18)
+//        return true;
+//    else
+//        return false;
+//}
+
+//Zad7 
+
+
+
+using System;
+using System.Runtime.InteropServices;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Wpisz nowe hasło: ");
+        string password = Console.ReadLine();
+
+        try
+        {
+            ValidePassword(password);
+
+            SetYourNewPassword(password);
+
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine("Błąd: " + ex.Message);
+        }
+
+        static void ValidePassword(string password)
+        {
+            {
+                if (string.IsNullOrEmpty(password)) throw new ArgumentException("Hasło nie może być puste, musi zawierać jakieś znaki");
+                if (password.Length < 10) throw new FormatException("Hasło musi mieć więcej niż 10 znaków");
+                if (!password.Any(char.IsUpper)) throw new FormatException("Hasło musi zawierać w sobie jedną dużą literę");
+                if (!password.Any(char.IsLower)) throw new FormatException("Hasło musi zawierać w sobie jedną małą literę");
+                if (!password.Any(char.IsDigit)) throw new FormatException("Hasło musi zawierać w sobie liczbę");
+                if (!password.Any(ch => "!@#$%^&*()".Contains(ch))) throw new FormatException("Hasło musi zawierać specjalny znak");
+            }
+        }
+
+        static void SetYourNewPassword(string password)
+        {
+            Console.WriteLine("Hasło ustawione pomyślnie!");
+        }
+    }
+}
+
+//Koniec Zad7
+
+
+//Przykład tłumaczący 
+
+//[Serializable]
+//public class InvalidDepartmentException : Exception
+//{
+//    public InvalidDepartmentException() : base() { }
+//    public InvalidDepartmentException(string message) : base(message) { }
+//    public InvalidDepartmentException(string message, Exception inner) : base(message, inner) { }
+//}
+
+//zad 13 (z pierwszych labów - równanie kwadratowe)
+
+//double[] QuadricFun(int a, int b, int c)
+//{
+//    // AX + BX + C
+//    //Obliczanie Delty
+
+//    return new double[2];
+//}
